56baa79bb76b4228a03aad47b6fe22b266da1979
Saphira3000 <gaby.saphira@gmail.com>
2020-09-15 11:17:41 -0500

    cambios

--- t2/t2.R
+++ t2/t2.R
@@ -50,14 +50,27 @@ palabrasf = as.data.frame(table(palabras$word))
 colnames(palabrasf) = c("palabra", "frec")
 palabrasf = palabrasf[palabrasf$frec > 1,]
 
-head(palabraso, 10)
-
 palabraso = palabrasf[order(palabrasf$frec, decreasing = T),]
+head(palabraso, 10)
 png('palabras_decreciente_log3.png', width=2600, height=1600, res=300)
 barplot(palabraso$frec[11:30], names.arg = palabraso$palabra[11:30], ylab = "Frecuencia", las=2)
 dev.off()
 
+# sentimientos sin filtro de frecuencias
+bing_count <- palabras %>% inner_join(get_sentiments("bing")) %>% count(word, sentiment, sort = TRUE) %>% ungroup()
 
-palabraso = Tpalabras[order(Tpalabras$frec, decreasing = T),]
-barplot(palabraso$frec, names.arg = palabraso$palabra, log = "y")
+positive = bing_count[bing_count$sentiment == "positive", ]
+positive = positive[positive$n > 1, ] # filtro frec > 1
+library(RColorBrewer)
+png('palabras_positivas.png', width=1600, height=1400, res=300)
+barplot(positive$n[1:10], names.arg = positive$word[1:10], ylab = "Frecuencia", las = 2, 
+        col = brewer.pal(10, "Set3"))
+dev.off()
+
+negative = bing_count[bing_count$sentiment == "negative", ]
+negative = negative[negative$n > 1, ] # filtro frec > 1
+png('palabras_negativas.png', width=1600, height=1400, res=300)
+barplot(negative$n[1:10], names.arg = negative$word[1:10], ylab = "Frecuencia", 
+        las = 2, col = grey.colors(10))
+dev.off()
 
