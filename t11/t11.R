17e3b431316847061dc01628f060a45608fcb984
Saphira3000 <gaby.saphira@gmail.com>
2020-11-17 03:58:48 -0600

    parte 2 y 3

---
+++ t11/t11.R
@@ -0,0 +1,51 @@
+
+# apoyo numerico para demostrar la primera propiedad 
+a = 3
+b = 1
+c = 5
+d = 2
+
+cov_abcd = numeric()
+cov_ac = numeric()
+for(r in 1:100) {
+  X = rnorm(1000, 0, 5)
+  Y = runif(1000)
+  cov_abcd = c(cov_abcd, cov(a*X+b, c*Y+d))
+  cov_ac = c(cov_ac, a*c*cov(X, Y))
+}
+png("cov_abcd.png", width=2000, height=1600, res=300)
+par(mar=c(3,4,1,1))
+hist(cov_abcd, main = "", ylab = "Frecuencia", xlab = " ", col = "slategray1")
+dev.off()
+png("cov_ac.png", width=2000, height=1600, res=300)
+par(mar=c(3,4,1,1))
+hist(cov_ac, main = "", ylab = "Frecuencia", xlab = " ", col = "lightpink")
+dev.off()
+
+
+# apoyo numerico para demostrar la segunda propiedad 
+var_suma = numeric()
+resultado = numeric()
+for(r in 1:100) {
+  X = rnorm(1000, 0, 5)
+  Y = runif(1000)
+  var_suma = c(var_suma, var(X+Y))
+  resultado = c(resultado, var(X) + var(Y) + 2*cov(X,Y))  
+}
+png("var_suma.png", width=2000, height=1600, res=300)
+par(mar=c(3,4,1,1))
+hist(var_suma, main = "", ylab = "Frecuencia", xlab = " ", col = "slategray1")
+dev.off()
+png("var_resultado.png", width=2000, height=1600, res=300)
+par(mar=c(3,4,1,1))
+hist(resultado, main = "", ylab = "Frecuencia", xlab = " ")
+dev.off()
+
+
+# prueba chi cuadrada
+muy_efectivo = c(16.3, 8.7)
+efectov = c(49.7, 45.1)
+prueba = as.table(cbind(muy_efectivo, efectov))
+colnames(prueba) <- c("muy efectivo","algo efectivo")
+rownames(prueba) <- c("Policia federal","policia estatal")
+chisq.test(prueba, correct = F)
